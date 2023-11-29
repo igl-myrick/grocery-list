94725e43114aad4693da51e4e914b760c1db5f56
igl-myrick <igl.myrick@gmail.com>
2023-11-29 14:44:45 -0800

    add list reset feature

--- js/scripts.js
+++ js/scripts.js
@@ -6,14 +6,27 @@ function sortList(arrayToSort) {
 // ui logic
 window.addEventListener("load", function() {
   let form = document.querySelector("form");
+  const mainPage = document.getElementById("mainPage")
   form.addEventListener("submit", function(e) {
     e.preventDefault();
+    mainPage.setAttribute("class", "hidden")
+
     const userInputs = document.querySelectorAll("input[name=groceries]:checked");
     const userList = Array.from(userInputs);
-
     const listHeading = document.createElement("h1"); 
     listHeading.append("Your list:");
     document.body.append(listHeading);
+    const ul = document.createElement("ul");
+    document.body.append(ul);
+    
+    let resetBtn = document.createElement("button");
+    resetBtn.setAttribute("type", "button");
+    resetBtn.append("Reset!");
+    document.body.append(resetBtn);
+
+    resetBtn.addEventListener("click", function() {
+      location.reload();
+    });
 
     let newList = [];
     userList.forEach(function(element) {
@@ -21,9 +34,6 @@ window.addEventListener("load", function() {
     });
 
     sortList(newList);
-
-    const ul = document.createElement("ul");
-    document.body.append(ul);
 
     newList.forEach(function(element) {
       const li = document.createElement("li");
